cfd5c3ae3b4fc713a357ce4d340cf475e5097e46
Anna Poplavska <annapoplavska10@gmail.com>
2025-03-23 10:08:41 +0100

    Added space

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,8 @@ export class App extends React.Component {
         <p className="App__message">
           {this.state.pressedKey
             ? `The last pressed key is [${this.state.pressedKey}]`
-            : 'Nothing was pressed yet'}
+            : 'Nothing was pressed yet'
+          }
         </p>
       </div>
     );
